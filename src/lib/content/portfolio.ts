4d1d97ad7370a6244e9026ec3ca1baf0ae13451e
farrelputra16 <farrelptrra16@students.unnes.ac.id>
2024-07-27 13:04:53 +0700

    tambah

--- src/lib/content/portfolio.ts
+++ src/lib/content/portfolio.ts
@@ -25,8 +25,8 @@ export const seoData = {
     'Farrel Arkesya Kahira Putra is a Software Engineer who specializes in building (and occasionally designing) exceptional visual interfaces.',
   author: author.name,
   image:
-    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
-  url: 'https://vatsalsinghkv.vercel.app/',
+    '  ',
+  url: 'https://farrel-putra16-portfolio.vercel.app/',
   keywords: [
     'Farrel',
     'Farrel Putra',
